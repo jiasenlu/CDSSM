d1b53f81e53a140ed9f01488611ce840919e553b
Jiasen Lu <echosenm@gmail.com>
2015-08-23 18:10:49 -0400

    update the criterion function.

--- Calculate_Alpha.lua
+++ Calculate_Alpha.lua
@@ -3,12 +3,8 @@ local Calculate_Alpha = {}
 Calculate_Alpha.__index = Calculate_Alpha
 
 function Calculate_Alpha.calCosDist(input)
-    local input1, input2, Q_Index, D_Index = input[1], input[2], input[3], input[4]
+    local input1, input2 = input[1], input[2]
     local self = {}
-    Q_Index = Q_Index:type('torch.LongTensor')
-    D_Index = D_Index:type('torch.LongTensor')
-    input1 = input1:index(1, Q_Index)
-    input2 = input2:index(1, D_Index)
     if input1:dim() == 1 then
         input1 = input1:view(1,-1)
         input2 = input2:view(1,-1)
@@ -63,20 +59,20 @@ function Calculate_Alpha.cal_alpha(alpha, nTrial, batchsize, gamma)
 end
 
 function Calculate_Alpha.cal_alpha_sum(alpha, nTrial, batchsize, gamma, init)
-    -- alpha[postive] = init + alpha[postive + negative]
+    -- alpha[postive] = init + alpha[postive + negative] (sum of all the (pos - negtive))
     local pos_alpha = alpha:sub(1,batchsize)
     local neg_alpha = alpha:sub(batchsize+1, -1)    
     local neg_alpha_split = neg_alpha:split(batchsize, 1)
     for i = 1, nTrial do
-      pos_alpha:add(neg_alpha_split[i], init)
+      pos_alpha:add(neg_alpha_split[i])
     end
-
+    pos_alpha:add(init)
     local new_alpha = torch.cat(pos_alpha, neg_alpha)
     return new_alpha
 end
 
 function Calculate_Alpha.cal_alpha_norm(alpha, nTrial, batchsize, gamma)
-    -- alpha[negative] = gamma * alpha[negative]./ alpha[positive]
+    -- alpha[negative] = gamma * alpha[negative]./ alpha[positive] (normalization)
     local pos_alpha = alpha:sub(1,batchsize)
     local neg_alpha = alpha:sub(batchsize+1, -1)
     local positive_array = torch.range(1,batchsize):type('torch.IntTensor')  
--- DSSM_MMI_Criterion.lua
+++ DSSM_MMI_Criterion.lua
@@ -19,32 +19,66 @@ function DSSM_MMI_Criterion:__init(batch_size, nTrail, gamma)
     end
 
     -- concatinate with the postive.
-    local postive_array = torch.range(1,batch_size):type('torch.IntTensor')
-    self.D_sampling_array = torch.cat(postive_array, negtive_array)
-    self.Q_sampling_array = torch.repeatTensor(postive_array, nTrail+1)
+    self.D_negtive_array = negtive_array
     self.gradInput = {torch.Tensor(), torch.Tensor()}
 end
 
 function DSSM_MMI_Criterion:updateOutput(input)
-    local Q_input, D_input = input[1], input[2]
+    
+    local Q_input, D_input = input[1],input[2]
+    local dimension = Q_input:size()[2]
+
+    self.alpha_buffer = torch.Tensor(self.batch_size * (self.nTrail+1)):zero()
+    self.Q_derivative_buffer = torch.Tensor(self.nTrail+1, self.batch_size, dimension):zero()
+    self.D_derivative_buffer = torch.Tensor(self.nTrail+1, self.batch_size, dimension):zero()
+
     -- 1: calculate the cosine distance for positive and negtive array
-    local alpha = Calculate_Alpha.calCosDist({Q_input, D_input, self.Q_sampling_array, self.D_sampling_array})
-    -- 2: calculate the alpha
-    alpha = Calculate_Alpha.cal_alpha(alpha, self.nTrail, self.batch_size, self.gamma)
-    alpha = Calculate_Alpha.cal_alpha_sum(alpha, self.nTrail, self.batch_size, self.gamma,1)
-    alpha = Calculate_Alpha.cal_alpha_norm(alpha, self.nTrail, self.batch_size, self.gamma)
-    alpha = Calculate_Alpha.cal_alpha_sum(alpha, self.nTrail, self.batch_size, self.gamma,0)
+    local CosDist = nn.CosineDistance()
+
+    self.alpha_buffer:sub(1, self.batch_size):copy(CosDist:forward({Q_input, D_input}))
+    local tmp_derivative = CosDist:backward({Q_input, D_input}, torch.Tensor(self.batch_size):fill(1))
+    self.Q_derivative_buffer:sub(1,1):copy(tmp_derivative[1])
+    self.D_derivative_buffer:sub(1,1):copy(tmp_derivative[2])
+
+    -- for negtive sampling
+    for i = 1,self.nTrail do
+        -- shuffle the input index based on negative sampling.
+        local D_input_neg = torch.Tensor(D_input:size()):zero()
+
+        -- construct the negative input
+        for j = 1,self.batch_size do
+            D_input_neg:select(1,j):copy(D_input:select(1,self.D_negtive_array[(i-1)*self.batch_size + j]))
+        end 
+
+        self.alpha_buffer:sub(i * self.batch_size+1, (i+1)*self.batch_size):copy(CosDist:forward({Q_input, D_input_neg}))
+        
+        local tmp_derivative = CosDist:backward({Q_input, D_input_neg}, torch.Tensor(self.batch_size):fill(1))
+        self.Q_derivative_buffer:sub(i+1,i+1):copy(tmp_derivative[1])
+        self.D_derivative_buffer:sub(i+1,i+1):copy(tmp_derivative[2])
+    end
+
+    -- 2: calculate the alpha 
+    self.alpha_buffer = Calculate_Alpha.cal_alpha(self.alpha_buffer, self.nTrail, self.batch_size, self.gamma)
+    self.alpha_buffer = Calculate_Alpha.cal_alpha_sum(self.alpha_buffer, self.nTrail, self.batch_size, self.gamma,1)
+    self.alpha_buffer = Calculate_Alpha.cal_alpha_norm(self.alpha_buffer, self.nTrail, self.batch_size, self.gamma)
+    self.alpha_buffer = Calculate_Alpha.cal_alpha_sum(self.alpha_buffer, self.nTrail, self.batch_size, self.gamma,0)
     -- 3: calculate the loss
     local err = 0
     local eps = 1.4e-45
     for i = 1, self.batch_size do
-        err = err + math.log(math.max(eps, (1+alpha[i] / math.max(self.gamma - alpha[i], eps))))
+        err = err + math.log(math.max(eps, (1+self.alpha_buffer[i] / math.max(self.gamma - self.alpha_buffer[i], eps))))
     end
     return err
 
 end
 
 function DSSM_MMI_Criterion:updateGradInput(input)
+    -- here, we didn't use the fomula in the paper, we jsut
+    -- j is the number of negative sampling
+    -- gradInput = Sum_j[alpha_buffer[j] + (delta_postivePair - delta_negtivePair)]
+
+
+
 
 
 end
